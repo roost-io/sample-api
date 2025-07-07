713cd7ac7e8378d98614a26fc340e46ed44303ee
roost-io <8110509+mgdevstack@users.noreply.github.com>
2025-07-08 01:41:50 +0530

    Add API Tests (Karate Framework, Azure Open AI) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/org/springframework/api_tests/CircleCiOaiSecurity/CircleCiOaiSecurityTest.java
@@ -0,0 +1,32 @@
+
+package org.springframework.api_tests.CircleCiOaiSecurity;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CircleCiOaiSecurityTest {
+
+	@Test
+	void testAll() {
+		String apihosturl = System.getenv().get("API_HOST_URL");
+		String apiauthusername = System.getenv().get("API_AUTH_USERNAME");
+		String api2authpassword = System.getenv().get("API2_AUTH_PASSWORD");
+		String circletoken = System.getenv().get("circle-token");
+		String circletoken = System.getenv().get("Circle-Token");
+		Results results = Runner.path("src/test/java/org/springframework/api_tests/CircleCiOaiSecurity")
+			.systemProperty("API_HOST_URL", apihosturl)
+			.systemProperty("API_AUTH_USERNAME", apiauthusername)
+			.systemProperty("API2_AUTH_PASSWORD", api2authpassword)
+			.systemProperty("circle-token", circletoken)
+			.systemProperty("Circle-Token", circletoken)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
